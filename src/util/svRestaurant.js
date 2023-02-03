4fa92c4cb2a502ce53099d022fbaca1bbd0da34b
baechlerno <noel.baechler@post.ch>
2023-02-03 17:15:27 +0100

    fixed mistake in title of menu

--- src/util/svRestaurant.js
+++ src/util/svRestaurant.js
@@ -25,7 +25,11 @@ const getMenus = (dayDoc) => {
 const getMenuObject = (menuDoc) => {
     return ({
         type: menuDoc.getElementsByClassName('menuline')[0]?.innerHTML ?? '',
-        title: menuDoc.getElementsByClassName('menu-title')[0].innerHTML.replace(/&shy;/g, ''),
+        title: menuDoc.getElementsByClassName('menu-title')[0].innerHTML
+            .replace(/&shy;/g, '')
+            .replace(/&quot;/g, '"') // replaces " quotes
+            .replace(/\n/g, ' ') // replaces \n (newline)
+            .replace(/<br\/>/g, ''), // replaces <br/> tags
         description: menuDoc.getElementsByClassName('menu-description')[0].innerHTML
             .replace(/<br\/>/g, '') // replaces <br/> tags
             .replace(/\n/g, ' ') // replaces \n (newline)
